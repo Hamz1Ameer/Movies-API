7d3ddad6842e7d0bc2d0a1376fca793b78e27df1
Mohammed Ameer Hamza <101882275+Hamz1Ameer@users.noreply.github.com>
2023-03-31 19:54:40 +0000

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
-import Alan from "./Alan";
+import Alan from "./Alan"; 
 
 // const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 const API_URL = `https://www.omdbapi.com/?i=tt3896198&apikey=66ea1086`;
